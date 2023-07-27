67ab482f565c0a6f4c6cf0818408086394dfc9b9
wosleymares <wesleyaddan@gmail.com>
2023-07-27 16:58:41 -0300

    atualização da pasta

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TesteComponent } from './components/teste/teste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TesteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
